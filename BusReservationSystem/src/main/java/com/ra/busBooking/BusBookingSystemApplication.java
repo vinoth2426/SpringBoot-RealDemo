e8f7a93fd424d70371615c8f7239f07b0877bca3
Vinoth <vinothvino2426@gmail.com>
2024-08-11 10:34:40 +0530

    New Porject

---
+++ BusReservationSystem/src/main/java/com/ra/busBooking/BusBookingSystemApplication.java
@@ -0,0 +1,12 @@
+package com.ra.busBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusBookingSystemApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BusBookingSystemApplication.class, args);
+	}
+
+}
