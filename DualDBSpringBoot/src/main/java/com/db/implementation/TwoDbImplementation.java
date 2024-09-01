9742e2ec2245af0d6750ad56f13216400c3ae083
Vinoth <vinothvino2426@gmail.com>
2024-09-01 08:51:34 +0530

    DualDB

---
+++ DualDBSpringBoot/src/main/java/com/db/implementation/TwoDbImplementation.java
@@ -0,0 +1,14 @@
+package com.db.implementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoDbImplementation {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwoDbImplementation.class, args);
+
+	}
+
+}
---
+++ DualDBSpringBoot/src/main/java/com/db/implementation/repo1/StudentRepository.java
@@ -0,0 +1,11 @@
+package com.db.implementation.repo1;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.db.implementation.entity1.Student;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer>{
+
+}
